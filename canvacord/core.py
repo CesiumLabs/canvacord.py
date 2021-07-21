94f3237232958aae1ba3a9ca0262643ce495dbe1
Samrid Pandit <samrid.pandit@gmail.com>
2021-07-22 01:50:23 +0545

    [update] Now the `ClientSession` can be passed or is initialized in core

--- canvacord/core.py
+++ canvacord/core.py
@@ -1,9 +1,12 @@
+from typing import Optional
+import aiohttp
 from canvacord.generator.fun import FunGenerator
 
 
 class Canvacord:
-    def __init__(self) -> None:
-        self._fun_client = FunGenerator()
+    def __init__(self, session: Optional[aiohttp.ClientSession] = None) -> None:
+        self._session = session or aiohttp.ClientSession()
+        self._fun_client = FunGenerator(self._session)
 
     @property
     def fun(self) -> FunGenerator:
