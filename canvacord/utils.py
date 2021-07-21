bab5679798ac6a301d0326b413084ae6f2adc1a5
Samrid Pandit <samrid.pandit@gmail.com>
2021-07-22 00:24:04 +0545

    [add] Adds `utils.py` with _parse_user utility

---
+++ canvacord/utils.py
@@ -0,0 +1,31 @@
+import io
+from functools import wraps
+from typing import TypeVar
+
+import aiohttp
+import discord
+from PIL import Image
+
+_T = TypeVar("_T")
+
+
+def _parse_user(f: _T) -> _T:
+    @wraps(f)
+    async def wrapper(generator, avatar, *args, **kwargs):
+        ses = aiohttp.ClientSession()
+
+        if isinstance(avatar, str):
+            async with ses.get(avatar) as response:
+                avatar = Image.open(io.BytesIO(await response.read())).convert("RGBA")
+
+        elif isinstance(avatar, (discord.Member, discord.User)):
+            async with ses.get(avatar.avatar_url) as response:
+                avatar = Image.open(io.BytesIO(await response.read())).convert("RGBA")
+        else:
+            if not isinstance(avatar, Image.Image):
+                raise Exception("User not found.")
+
+        await ses.close()
+        return await f(generator, avatar, *args, **kwargs)
+
+    return wrapper
