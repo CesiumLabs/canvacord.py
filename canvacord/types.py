71d4c48910262cccfa79351d8eb846ca7dbaf0ad
Samrid Pandit <samrid.pandit@gmail.com>
2021-07-23 02:12:20 +0545

    [add] Adds `bytes` and `io.BytesIO` in the UserType

--- canvacord/types.py
+++ canvacord/types.py
@@ -1,7 +1,8 @@
+import io
 from typing import Union
 
 import discord
 from PIL import Image
 
-UserType = Union[discord.Member, discord.User, str, Image.Image]
+UserType = Union[discord.Member, discord.User, str, Image.Image, bytes, io.BytesIO]
 ImageCacheDict = dict[str, Image.Image]
