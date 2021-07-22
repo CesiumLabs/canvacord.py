fab1b95feec897dafa2cb91ac97827e7a20ab9bf
Samrid Pandit <samrid.pandit@gmail.com>
2021-07-23 02:11:57 +0545

    [add] Adds support for `bytes` and `io.BytesIO` in the _user_parser method

--- canvacord/helper/utils.py
+++ canvacord/helper/utils.py
@@ -1,23 +1,38 @@
 import io
 from functools import wraps
+import re
 from typing import TypeVar
 
 import httpx
 import discord
 from PIL import Image
+from yarl import URL
 
 from canvacord.types import UserType
 
 _T = TypeVar("_T")
 
+URL_REGEX = re.compile(
+    "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
+)
 
-async def _user_parser(avatar: UserType, async_client: httpx.AsyncClient) -> Image.Image:
+
+async def _user_parser(
+    avatar: UserType, async_client: httpx.AsyncClient
+) -> Image.Image:
     if isinstance(avatar, str):
-        avatar = Image.open(io.BytesIO((await async_client.get(avatar)).read()))
+        if URL_REGEX.findall(avatar):
+            return Image.open(io.BytesIO((await async_client.get(avatar)).read()))
+        return Image.open(avatar)
 
     elif isinstance(avatar, (discord.Member, discord.User)):
-        async with async_client.get(avatar.avatar_url) as response:
-            avatar = Image.open(io.BytesIO(await response.read())).convert("RGBA")
+        return Image.open(io.BytesIO(await avatar.avatar_url.read()))
+
+    elif isinstance(avatar, bytes):
+        return Image.open(io.BytesIO(bytes))
+
+    elif isinstance(avatar, io.BytesIO()):
+        return Image.open(avatar)
 
     elif not isinstance(avatar, Image.Image):
         raise Exception("User not found.")
