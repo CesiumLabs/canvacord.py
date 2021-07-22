75aad501dcb9fe247092d505744f5c1e3a01d190
Samrid Pandit <samrid.pandit@gmail.com>
2021-07-22 12:21:40 +0545

    [add] Adds `hitler` manipulation test

--- tests/fun_test.py
+++ tests/fun_test.py
@@ -1,3 +1,5 @@
+from typing import Callable
+import pathlib
 import sys
 
 import aiohttp
@@ -9,15 +11,31 @@ from PIL import Image
 
 from canvacord import Canvacord
 
+USER_AV = "https://cdn.discordapp.com/avatars/438741869107871775/d8383a718f0574e7dc6670e951a6ba4b.png?size=256"
+
+
+async def fun_tester(method: Callable, image_path: pathlib.Path):
+    image = await method(USER_AV)
+    first_image = Image.open(image)
+    second_image = Image.open(image_path)
+    return first_image, second_image
+
 
 @pytest.mark.asyncio
 async def test_jail():
     async with aiohttp.ClientSession() as session:
         canvacord = Canvacord(session)
-        image = await canvacord.fun.jail(
-            "https://cdn.discordapp.com/avatars/438741869107871775/d8383a718f0574e7dc6670e951a6ba4b.png?size=256"
+        first_image, second_image = await fun_tester(
+            canvacord.fun.jail, "tests/assets/jail.png"
         )
-        frist_image = Image.open(image)
-        second_image = Image.open("tests/assets/jail.png")
+    assert first_image == second_image
 
-        assert frist_image == second_image
+
+@pytest.mark.asyncio
+async def test_hitler():
+    async with aiohttp.ClientSession() as session:
+        canvacord = Canvacord(session)
+        first_image, second_image = await fun_tester(
+            canvacord.fun.hitler, "tests/assets/hitler.png"
+        )
+    assert first_image == second_image
