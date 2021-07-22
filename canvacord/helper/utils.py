fe7ab5385e8a6b8181ddabb98ac4341fb8d2cdb4
Samrid Pandit <samrid.pandit@gmail.com>
2021-07-22 22:59:01 +0545

    [update] Changed the name from `@manipulation` to `@args_parser`

--- canvacord/helper/utils.py
+++ canvacord/helper/utils.py
@@ -14,29 +14,29 @@ _T = TypeVar("_T")
 async def _user_parser(avatar: UserType, session: aiohttp.ClientSession) -> Image.Image:
     if isinstance(avatar, str):
         async with session.get(avatar) as response:
-            new_avatar = Image.open(io.BytesIO(await response.read())).convert("RGBA")
+            avatar = Image.open(io.BytesIO(await response.read())).convert("RGBA")
 
     elif isinstance(avatar, (discord.Member, discord.User)):
         async with session.get(avatar.avatar_url) as response:
-            new_avatar = Image.open(io.BytesIO(await response.read())).convert("RGBA")
-    else:
-        if not isinstance(avatar, Image.Image):
-            raise Exception("User not found.")
+            avatar = Image.open(io.BytesIO(await response.read())).convert("RGBA")
 
-    return new_avatar
+    elif not isinstance(avatar, Image.Image):
+        raise Exception("User not found.")
+
+    return avatar
 
 
-def _image_to_bytesio(image: Image.Image, format: str = "PNG"):
+def image_to_bytesio(image: Image.Image, format: str = "PNG"):
     b = io.BytesIO()
     image.save(b, format=format)
     b.seek(0)
     return b
 
 
-def manipulation(func: _T) -> _T:
+def args_parser(func: _T) -> _T:
     @wraps(func)
     async def wrapper(gen: type, *args, **kwargs):
-        session = gen._session
+        session = gen.session
         args = list(args)
 
         for index, arg in enumerate(args):
@@ -47,7 +47,6 @@ def manipulation(func: _T) -> _T:
             if isinstance(arg, UserType.__args__):
                 kwargs[key] = await _user_parser(value, session)
 
-        image = await func(gen, *args, **kwargs)
-        return _image_to_bytesio(image)
+        return await func(gen, *args, **kwargs)
 
     return wrapper
