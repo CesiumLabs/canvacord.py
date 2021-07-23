b8b51eb58ada183e176800f6ea1236d857ed2d19
Ammar-sys <amarftw1@gmail.com>
2021-07-23 20:49:21 +0200

    use aiohttp

--- canvacord/core.py
+++ canvacord/core.py
@@ -1,13 +1,13 @@
 from typing import Optional
 
-import httpx
+import aiohttp
 
 from canvacord.generator import FunGenerator
 
 
 class Canvacord:
-    def __init__(self, async_client: Optional[httpx.AsyncClient] = None) -> None:
-        self._async_client = async_client or httpx.AsyncClient()
+    def __init__(self, async_client: Optional[aiohttp.ClientSession] = None) -> None:
+        self._async_client = async_client or aiohttp.ClientSession()
         self._fun_client = FunGenerator(self._async_client)
 
     @property
