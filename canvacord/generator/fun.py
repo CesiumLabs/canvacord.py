0ccc947bc3505e7d35b0c4a19bff1cd786bf27e0
Samrid Pandit <samrid.pandit@gmail.com>
2021-07-24 18:35:25 +0545

    [update] Updates the manipulation methods to send co-ordinates instead of arguments

--- canvacord/generator/fun.py
+++ canvacord/generator/fun.py
@@ -17,8 +17,7 @@ class FunGenerator:
     async def jail(self, user: UserType) -> io.BytesIO:
         avatar = await asyncio.to_thread(
             self.image_helper.manipulate_image,
-            x=0,
-            y=0,
+            co_ordinates=(0, 0),
             background=user,
             back_size=(512, 512),
             foreground=self.image_helper.images_cache["jail.png"],
@@ -30,8 +29,7 @@ class FunGenerator:
     async def gay(self, user: UserType) -> io.BytesIO:
         avatar = await asyncio.to_thread(
             self.image_helper.manipulate_image,
-            x=0,
-            y=0,
+            co_ordinates=(0, 0),
             background=user,
             back_size=(512, 512),
             foreground=self.image_helper.images_cache["gay.png"],
@@ -44,8 +42,7 @@ class FunGenerator:
     async def jokeoverhead(self, user: UserType) -> io.BytesIO:
         avatar = await asyncio.to_thread(
             self.image_helper.manipulate_image,
-            x=150,
-            y=150,
+            co_ordinates=(150, 150),
             background=self.image_helper.images_cache["jokeoverhead.png"],
             foreground=user,
             fore_size=(95, 95),
@@ -56,8 +53,7 @@ class FunGenerator:
     async def hitler(self, user: UserType) -> io.BytesIO:
         avatar = await asyncio.to_thread(
             self.image_helper.manipulate_image,
-            x=40,
-            y=28,
+            co_ordinates=(40, 28),
             background=self.image_helper.images_cache["hitler.png"],
             foreground=user,
             fore_size=(153, 153),
@@ -68,16 +64,14 @@ class FunGenerator:
     async def spank(self, user1: UserType, user2: UserType) -> io.BytesIO:
         first_image = await asyncio.to_thread(
             self.image_helper.manipulate_image,
-            x=460,
-            y=100,
+            co_ordinates=(460, 100),
             background=self.image_helper.images_cache["spank.png"],
             foreground=user1,
             fore_size=(180, 180),
         )
         second_image = await asyncio.to_thread(
             self.image_helper.manipulate_image,
-            x=710,
-            y=480,
+            co_ordinates=(710, 480),
             background=first_image,
             foreground=user2,
             fore_size=(180, 180),
@@ -88,8 +82,7 @@ class FunGenerator:
     async def wanted(self, user: UserType) -> io.BytesIO:
         avatar = await asyncio.to_thread(
             self.image_helper.manipulate_image,
-            x=269,
-            y=451,
+            co_ordinates=(269, 451),
             background=self.image_helper.images_cache["wanted.png"],
             foreground=user,
             fore_size=(395, 395),
