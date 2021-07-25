eb002f50bfe3aee0508752e5bc02e8fc1b64dba2
Ammar-sys <amarftw1@gmail.com>
2021-07-25 02:08:45 +0200

    [adds] new typing var `fontcachedict`

--- canvacord/types.py
+++ canvacord/types.py
@@ -2,7 +2,8 @@ import io
 from typing import Union
 
 import discord
-from PIL import Image
+from PIL import Image, ImageFont
 
 UserType = Union[discord.Member, discord.User, str, Image.Image, bytes, io.BytesIO]
 ImageCacheDict = dict[str, Image.Image]
+FontCacheDict = dict[str, ImageFont.truetype]
