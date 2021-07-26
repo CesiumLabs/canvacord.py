ebbd6607e66e0d54d5a9eaf519fcb71af8aa6247
Ammar-sys <amarftw1@gmail.com>
2021-07-26 18:10:53 +0200

    [update] add docstrings and improve fontcache

--- canvacord/helper/cache.py
+++ canvacord/helper/cache.py
@@ -1,4 +1,7 @@
+"""Classes for caching purposes (images, fonts as of now)."""
 import os
+import io
+
 from pathlib import Path
 from typing import Optional
 
@@ -12,14 +15,29 @@ FONT_ASSET_DIRECTORY = Path("canvacord/assets/fonts")
 
 class ImageCache:
     def __init__(
-        self, directory: Optional[str] = None, cache_dict: Optional[dict] = None
+        self, directory: Optional[str] = None, cache_dict: Optional[ImageCacheDict] = None
     ) -> None:
+        """
+        Initialize class variables as well as load images from provided directory.
+
+        :param directory: directory to load images from
+        :rtype directory: Optional[str]
+        :param cache_dict: premade dict of images
+        :rtype cache_dict: Optional[ImageCacheDict]
+        """
         self.directory = directory or IMAGE_ASSET_DIRECTORY
         self.images_cache = cache_dict or {}
 
         self.load_images(self.directory)
 
     def load_images(self, directory: str) -> ImageCacheDict:
+        """
+        Load images from provided directory.
+
+        :param directory: dir to load images from
+        :rtype: str
+        :return: ImageCacheDict
+        """
         images_cache: ImageCacheDict = {
             item: Image.open(directory / item).convert("RGBA")
             for item in os.listdir(directory)
@@ -33,20 +51,45 @@ class ImageCache:
 
 class FontCache:
     def __init__(
-        self, directory: Optional[str] = None, cache_dict: Optional[dict] = None
+        self, directory: Optional[str] = None, cache_dict: Optional[FontCacheDict] = None
     ) -> None:
+        """
+        Initialize class variables as well load fonts.
+
+        Class to be used in the following way: FontCache[(font, size)]
+
+        :param directory: directory to load fonts from
+        :rtype directory: Optional[str]
+        :param cache_dict: a premade fontcache dict
+        :rtype cache_dict: Optional[dict]
+        """
         self.directory = directory or FONT_ASSET_DIRECTORY
-        self.font_cache = cache_dict or {}
+        self.filelike = cache_dict or {}
 
         self.load_fonts(self.directory)
 
-    def load_fonts(self, directory: str, size: int = 10) -> FontCacheDict:
-        font_cache: ImageCacheDict = {
-            item: ImageFont.truetype(directory / item, size=size)
-            for item in os.listdir(directory)
-            if item.endswith(".ttf")
-        }
+    def load_fonts(self, directory: str) -> FontCacheDict:
+        """
+        Load fonts and cache them in BytesIO.
 
-        self.font_cache = self.font_cache | font_cache
+        :param directory: directory to load from
+        :rtype directory: str
+        :return: FontCacheDict
+        """
+        filelike: FontCacheDict = {}
 
-        return self.font_cache
+        for item in os.listdir(directory):
+            if item.endswith(".ttf"):
+                f = open(directory / item, 'rb')
+                filelike[item] = io.BytesIO(f.read())
+                f.close()
+
+        self.filelike = self.filelike | filelike
+
+        return self.filelike
+
+    def __getitem__(self, key: tuple[str, int]) -> ImageFont.FreeTypeFont:
+        return ImageFont.truetype(self.filelike[key[0]], size=key[1])
+
+    def __setitem__(self, key, value):
+        self.filelike[key] = value
