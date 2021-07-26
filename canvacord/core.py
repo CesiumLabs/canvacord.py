f2dfaa1fffe468d8f5f863def2f545656eaf7e5f
Ammar-sys <amarftw1@gmail.com>
2021-07-26 14:42:21 +0200

    [update] add docstrings

--- canvacord/core.py
+++ canvacord/core.py
@@ -1,3 +1,4 @@
+"""The main file with all methods for the user to use."""
 from typing import Optional
 
 import aiohttp
@@ -7,6 +8,12 @@ from canvacord.generator import FunGenerator
 
 class Canvacord:
     def __init__(self, async_session: Optional[aiohttp.ClientSession] = None) -> None:
+        """
+        The brains of the operation. Initialize class variables.
+
+        :param async_session: aiohttp session, if none, create one which must be closed.
+        :rtype async_session: Optional[aiohttp.ClientSession]
+        """
         self.async_session = async_session or aiohttp.ClientSession()
         self._fun_client = FunGenerator(self.async_session)
 
