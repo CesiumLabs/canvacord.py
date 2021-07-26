33ef7c9886e77080ecf1bd3496effce3ed19819d
Ammar-sys <amarftw1@gmail.com>
2021-07-26 14:51:01 +0200

    [patch] fix annotations

--- canvacord/helper/utils.py
+++ canvacord/helper/utils.py
@@ -1,9 +1,13 @@
+"""Helper functions for the image-method."""
 import io
 import re
-from typing import TYPE_CHECKING, Union
+from typing import TYPE_CHECKING, TypeVar
+from typing_extensions import ParamSpec, Literal
+from collections.abc import Callable, Awaitable
 
+from canvacord.types import UserType
 if TYPE_CHECKING:
-    from canvacord.generator import FunGenerator, RankCard, WelcomeCard
+    from canvacord.generator import FunGenerator, RankCard, WelcomeCard, BoostCard
 
 from functools import wraps
 
@@ -11,16 +15,25 @@ import aiohttp
 import discord
 from PIL import Image
 
-from canvacord.types import UserType
-
 URL_REGEX = re.compile(
     "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*(),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
 )
 
+T = TypeVar("T")
+P = ParamSpec("P")
 
 async def _user_parser(
     avatar: UserType, async_session: aiohttp.ClientSession
 ) -> Image.Image:
+    """
+    Parse the input user provides for the avatar argument.
+
+    :param avatar: avatar to parse
+    :rtype avatar: UserType
+    :param async_session: aiohttp session to be used
+    :rtype async_session: aiohttp.ClientSession
+    :return:
+    """
     if isinstance(avatar, str):
         if URL_REGEX.findall(avatar):
             async with async_session.get(avatar) as resp:
@@ -43,28 +56,45 @@ async def _user_parser(
 
 
 def image_to_bytesio(image: Image.Image, imgformat: str = "PNG") -> io.BytesIO:
+    """
+    Convert an image to bytesio.
+
+    :param image: Image to convert to bytesio.
+    :rtype image: Image.Image
+    :param imgformat: Format for the image, default, PNG.
+    :rtype imgformat: str
+    :return: io.BytesIO
+    """
     b = io.BytesIO()
     image.save(b, format=imgformat)
     b.seek(0)
     return b
 
 
-def args_parser(func):
+def args_parser(func: Callable[P, Awaitable[T]]) -> Callable[P, Awaitable[T]]:
     @wraps(func)
     async def wrapper(
-        gen: Union["FunGenerator", "RankCard", "WelcomeCard"], *args, **kwargs
-    ):
-        async_session = gen.async_session
-        args = list(args)
+        gen: Literal['FunGenerator', 'RankCard', 'WelcomeCard', 'BoostCard'], *args: P.args, **kwargs: P.kwargs
+    ) -> T:
+        """
+        Call _user_parser() func on all arguments annotated with the UserType type.
 
-        for index, arg in enumerate(args):
+        :param gen: gen which must include the async_session
+        :rtype gen: Literal['FunGenerator', 'RankCard', 'WelcomeCard', 'BoostCard']
+        :param args: arguments
+        :param kwargs: keyword arguments
+        :return: T
+        """
+        async_session = gen.async_session
+        arguments = list(args)
+
+        for index, arg in enumerate(arguments):
             if isinstance(arg, UserType.__args__):
-                args[index] = await _user_parser(arg, async_session)
+                arguments[index] = await _user_parser(arg, async_session)
 
         for key, value in kwargs.items():
             if isinstance(arg, UserType.__args__):
                 kwargs[key] = await _user_parser(value, async_session)
 
-        return await func(gen, *args, **kwargs)
-
+        return await func(gen, *arguments, **kwargs)
     return wrapper
