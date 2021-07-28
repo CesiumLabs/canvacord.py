4af6aafc8479b22c1cf7c34bc0353762ddacdcae
Samrid Pandit <samrid.pandit@gmail.com>
2021-07-28 12:21:39 +0545

    [add] Adds `base.py` with `BaseGenerator` and `CardGenerator`

---
+++ canvacord/generator/base.py
@@ -0,0 +1,33 @@
+import abc
+import io
+from typing import Optional
+
+import aiohttp
+
+from canvacord.helper.image import ImageHelper
+from canvacord.helper.utils import aioify
+
+
+class BaseGenerator(abc.ABC):
+    def __init__(
+        self,
+        async_session: Optional[aiohttp.ClientSession],
+        image_helper: Optional[ImageHelper] = None,
+    ):
+        """
+        Initialize class variables.
+
+        :param async_session: async session to be used
+        :rtype async_session: aiohttp.ClientSession
+        :param image_helper: image helper
+        :rtype image_helper: ImageHelper
+        """
+        self.async_session = async_session if async_session else aiohttp.ClientSession()
+        self.image_helper = image_helper if image_helper else ImageHelper()
+
+
+class CardGenerator(BaseGenerator):
+    @abc.abstractmethod
+    @aioify
+    async def create(self) -> io.BytesIO:
+        pass
