d61427fe142cfbc887b4940442986737b2a2d302
Samrid Pandit <samrid.pandit@gmail.com>
2021-07-28 12:24:46 +0545

    Revert "[update] `BoostCard` now imports `CardGenerator`"
    This reverts commit 089efb7982f19095b7e43841d0770e8648fb5cac.

--- canvacord/generator/boostcard.py
+++ canvacord/generator/boostcard.py
@@ -1,8 +1,34 @@
-from canvacord.generator.base import CardGenerator
-from canvacord.helper.utils import aioify
+import aiohttp
+
+from canvacord.helper.image import ImageHelper
 
 
-class BoostCard(CardGenerator):
-    @aioify
-    async def create(self, boosts: int, booster_name: str):
+class BoostCard:
+    def __init__(
+        self,
+        async_session: aiohttp.ClientSession,
+        image_helper: ImageHelper,
+        boosts: int,
+        booster_name: str,
+    ) -> None:
+        """
+        Initialize class variables.
+
+        :param async_session: async session to be used
+        :rtype async_session: aiohttp.ClientSession
+        :param image_helper: image helper
+        :rtype image_helper: ImageHelper
+        :param boosts: number of boosts a server has
+        :rtype boosts: int
+        :param booster_name: name of the booster including discriminator
+        :rtype booster_name: str
+        """
+        self.image_helper = image_helper
+        self.async_session = async_session
+        self.boosts = boosts
+        self.booster_name = booster_name
+
+        self.create()
+
+    def create(self):
         """TODO: Create the image."""
